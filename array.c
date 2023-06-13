d48f24129ee07ca7c0736dc670fd3bafa2c5dd60
Prarpita <135800001+Prarpita@users.noreply.github.com>
2023-06-13 19:20:18 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,59 @@
+/*#include<stdio.h>
+int main()
+{
+    int matrix[50][50]={1,2,3};
+    //matrix[0][0]=7;
+    printf("%d",matrix[0][0]);
+    return 0;
+}*/
+
+
+/*#include <stdio.h>
+
+int main() {
+
+  int values[5];
+
+  printf("Enter 5 integers: ");
+
+  
+  for(int i = 0; i < 5; ++i) {
+     scanf("%d", &values[i]);
+  }
+
+  printf("Displaying integers: ");
+
+  
+  for(int i = 0; i < 5; ++i) {
+     printf("%d\n", values[i]);
+  }
+  return 0;
+}*/
+
+
+#include <stdio.h>
+
+int main() {
+
+  int marks[10], i, n, sum = 0;
+  double average;
+
+  printf("Enter number of elements: ");
+  scanf("%d", &n);
+
+  for(i=0; i < n; ++i) {
+    printf("Enter number%d: ",i+1);
+    scanf("%d", &marks[i]);
+          
+    
+    sum += marks[i];
+  }
+
+  
+  average = (double) sum / n;
+
+  printf("Average = %.2lf", average);
+
+  return 0;
+}
+
